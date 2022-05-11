6a5b5eaccce492a52eb0aea3bde1623dae7bc7cc
kenneth-kip <kipyegonkeneth@gmail.com>
2022-05-11 17:30:39 +0300

    namespace the transaction routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :transactions, only: [:index, :create, :show]
+  namespace :api do
+    namespace :v1 do
+      resources :transactions, only: [:index, :create, :show]
+    end
+  end
 end
